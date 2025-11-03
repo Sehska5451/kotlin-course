5749c3fec78e51b4e2aa258e191d972804cdea1e
Kugrashov_A <senya160@gmail.com>
2025-11-03 16:51:39 +0400

    Homework15

--- src/main/kotlin/lessons/lesson15/homework/Furniture.kt
+++ src/main/kotlin/lessons/lesson15/homework/Furniture.kt
@@ -46,10 +46,41 @@ class takeFirstMaterial: Materials() {
 }
 
 //Второй класс должен получать список строк и вставлять их в начало контейнера но поочерёдно с теми данными, которые уже есть. То-есть, наш список должен появиться в контейнере по индексам 0, 2, 4 и так далее.
-class
+class InsertOneByOneMaterials : Materials() {
+
+    fun insert(items: List<String>) {
+        val materials = extractMaterials()
+        val maxIndex = max(items.lastIndex, materials.lastIndex)
+        for (i in 0..maxIndex) {
+            if (i < items.size) {
+                addMaterial(items[i])
+            }
+            if (i < materials.size) {
+                addMaterial(materials[i])
+            }
+        }
+    }
+}
 //Третий класс при добавлении строки должен отсортировать весь список метиралов в алфавитном порядке включая добавляемый материал.
+class InsertSortedMaterials : Materials() {
+
+    fun insert(item: String) {
+        val materials = (extractMaterials() + item).sorted()
+        materials.forEach {
+            addMaterial(it)
+        }
+    }
+}
 
 //Четвёртый класс должен принимать словарь (ключи и значения строки).
 // Каждая пара ключа и значения должна попадать в контейнер следующим образом - ключ в начало, значение в конец.
 // Таким образом в начале должны быть ключи в обратном порядке, потом существующие материалы, потом все значения.
+class InsertMapMaterials : Materials() {
 
+    fun insert(items: Map<String, String>) {
+        val materials = items.keys.reversed() + extractMaterials() + items.values
+        materials.forEach {
+            addMaterial(it)
+        }
+    }
+}

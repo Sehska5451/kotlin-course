1b6d7aaf57a351887f1922a3b45f1e0221f44cfa
Kugrashov_A <senya160@gmail.com>
2025-12-08 18:27:39 +0400

    Homework25

---
+++ src/main/kotlin/lessons/lesson25/Homework25.kt
@@ -0,0 +1,103 @@
+package main.kotlin.lessons.lesson25
+
+//Задание 1. Функция высшего порядка.
+//Создайте функцию timeTracker, которая принимает другую функцию в качестве аргумента и измеряет время её выполнения.
+// Функция timeTracker должна возвращать затраченное время в миллисекундах.
+// Для измерения времени используйте System.currentTimeMillis() до и после выполнения переданной функции.
+
+fun timeTracker(block: () -> Unit): Long {
+    val start = System.currentTimeMillis()
+    block()
+    val end = System.currentTimeMillis()
+    return end - start
+}
+
+//Задания для работы со scope функциями
+//Давайте создадим два класса, которые будут использоваться в заданиях для демонстрации работы scope функций с преобразованием объектов
+// и инициализацией или обновлением полей.
+// Классы будут представлять собой Person и Employee, где Employee может быть создан на основе данных класса Person.
+
+//Классы для выполнения заданий
+
+class Person(val name: String, val age: Int) {
+    var email: String = ""
+}
+
+class Employee(val name: String, val age: Int, val position: String) {
+    var email: String = ""
+    var department: String = "General"
+}
+
+//Задание 3: Использование also для логирования
+//
+//Создайте функцию, принимающую объект Person и выводящую информацию об объекте в консоль в красивом, отформатированном виде.
+// Используй with для создания контекста Person и возможности использовать поля объекта напрямую без обращения к переменной.
+
+fun logPerson(person: Person) = person.also {
+    with(it) {
+        println()
+    }
+}
+
+//Задание 6: Использование let для безопасного преобразования Person в Employee
+//
+//Создайте функцию toEmployee, которая расширяет класс Person, который может быть null.
+// В функции используйте функцию let для создания Employee, только если объект Person не null.
+// В противном случае возвращается null
+
+fun Person?.toEmployee(position: String): Employee? {
+    return this?.let { person ->
+        Employee(person.name, person.age, position).apply {
+            email = person.email
+            department = "General"
+        }
+    }
+}
+
+fun main() {
+    val myFunction = {
+        val list = List(10000000) { (0..10000).random() }
+        list.sorted()
+    }
+
+//Задание 2: Использование apply для инициализации объекта Employee
+
+    val person = Person("Andrew", 25).apply {
+        email = "Adnrew_help@bishkek.com"
+    }
+
+    val employee = Employee(person.name, person.age, "QA_engineer").apply {
+        email = person.email
+        department = "Web_development"
+    }
+
+    val person1 = Person("Jamal", 28)
+        .also { it.email = "Jamal@test.com" }
+        .also { logPerson(it) }
+
+    logPerson(person)
+
+//Задание 4: Использование with для преобразование Person в Employee
+//
+//Создайте объект класса Person. Используйте функцию with, чтобы создать из этого объекта Employee, указав дополнительно должность.
+// Возвращаемым значением должен быть новый объект Employee.
+
+val employee1 = with(person) {
+    Employee(name, age, "QA_engineer").apply {
+        email = this@with.email
+        department = "IT"
+    }
+}
+
+//Задание 5: Использование run для преобразования и инициализации Employee
+//
+//Перепиши предыдущую задачу на использование run вместо with.
+
+    val employee2 = person.run {
+        Employee(name, age, "Developer").apply {
+            email = this@run.email
+            department = "IT"
+        }
+    }
+}
+
